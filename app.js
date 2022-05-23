7b2adac512d66425dbdac2d0f2e7d87fb08ef140
unknown <dcalper00@gmail.com>
2022-05-23 19:20:25 +0200

    primer commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require("cors");
+const app = express()
+const port = 3000
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Endpoint principal!')
+})
+
+app.get('/login', (req, res) => {
+    
+    const username = req.query.username;
+    const password = req.query.password;
+
+    if(username == 'danicala' && password == 'pass123'){
+      res.send({
+        'status': 'OK',
+        'token': 'token123',
+        'details': 'Login completed succsesfully!'
+      });
+    } else {
+      res.send({
+        'status': 'NOK',
+        'token': '',
+        'details': 'Incorrect user or password!'
+      });
+    }
+
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
